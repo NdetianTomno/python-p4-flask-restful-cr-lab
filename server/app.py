b1ad9bfb7f55da50220a932241433f53579c1bb8
Tomno <tomnoviv@gmail.com>
2023-07-02 13:38:42 +0300

    Complete

--- server/app.py
+++ server/app.py
@@ -9,19 +9,44 @@ from models import db, Plant
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        title = "Plants Page"
+        plants = Plant.query.all()
+        serialized_plants = [plant.serialize() for plant in plants]
+        return jsonify(title=title, plants=serialized_plants)
+
+    def post(self):
+        data = request.get_json()
+        plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(plant)
+        db.session.commit()
+        return jsonify(plant), 201
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        if plant:
+            return jsonify(plant)
+        else:
+            return make_response(jsonify(error='Plant not found'), 404)
+
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,7 +3,18 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
+class Plant(db.Model):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'price': self.price
+        }
